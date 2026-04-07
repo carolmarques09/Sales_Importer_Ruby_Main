927bc3e55346cf038403d79e9c9b470eb4634e62
Carol <162912256+carolmarques09@users.noreply.github.com>
2026-04-07 10:15:58 -0300

    Add files via upload

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,1 +1,20 @@
+require 'csv'
 
+class Parser
+  def self.parse(file_path)
+    data = []
+
+    CSV.foreach(file_path, headers: true) do |row|
+      data << {
+        purchaser_name: row['purchaser name'],
+        item_description: row['item description'],
+        item_price: row['item price'].to_f,
+        purchase_count: row['purchase count'].to_i,
+        merchant_address: row['merchant address'],
+        merchant_name: row['merchant name']
+      }
+    end
+
+    data
+  end
+end
