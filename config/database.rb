f61a03acd3a55d42817f9c9ca8d34befa011b490
Carol <162912256+carolmarques09@users.noreply.github.com>
2026-04-07 10:14:47 -0300

    Add files via upload

--- config/database.rb
+++ config/database.rb
@@ -1,1 +1,11 @@
+require "active_record"
+require "yaml"
 
+env = ENV["RACK_ENV"] || "development"
+
+# Load DB config
+config_file = File.join(__dir__, "database.yml")
+config = YAML.safe_load(File.read(config_file), aliases: true)
+
+# Connect to DB
+ActiveRecord::Base.establish_connection(config[env])
